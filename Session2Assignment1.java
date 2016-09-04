17e7fc439b9204a3ecce9223d992057503fd7fba
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-09-04 18:46:15 +0530

    Hi

---
+++ Session2Assignment1.java
@@ -0,0 +1,13 @@
+public class Session2Assignment1
+{
+    public static void main(String [] args)
+    {
+         if(System.out.printf("Ashish Bansal") == null) 
+          {
+ 
+          }
+
+     }
+}
+
+
